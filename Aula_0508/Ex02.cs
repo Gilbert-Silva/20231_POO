02f6ae2a4af41af5084d942f180478184b947d34
Replit user <>
2023-05-08 18:59:39 +0000

    Aula 0805

--- Aula_0508/Ex02.cs
+++ Aula_0508/Ex02.cs
@@ -4,14 +4,24 @@ class Program {
     Cliente a = new Cliente("Gilbert", 1000);
     Cliente b = new Cliente("Jorgiano", 1500);
     Cliente c = new Cliente("Eduardo", 2000);
+    Cliente d = new Cliente("Marília", 2000);
     a.SetSocio(b);
     Console.WriteLine(a);
     Console.WriteLine(b);
     Console.WriteLine(c);
+    Console.WriteLine(d);
+    Console.WriteLine();
+    c.SetSocio(d);
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+    Console.WriteLine(c);
+    Console.WriteLine(d);
+    Console.WriteLine();
     a.SetSocio(c);
     Console.WriteLine(a);
     Console.WriteLine(b);
     Console.WriteLine(c);
+    Console.WriteLine(d);   
   }
 }
 class Cliente {
@@ -23,6 +33,10 @@ class Cliente {
     if (limite > 0) this.limite = limite;
   }
   public void SetSocio(Cliente x) {
+    Cliente sociodeThis = this.socio;
+    Cliente sociodeX = x.socio;
+    if (sociodeThis != null) sociodeThis.socio = null;
+    if (sociodeX != null) sociodeX.socio = null;
     this.socio = x;
     x.socio = this;
   }
